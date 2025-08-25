112623e1d037cb1371f2ac9dbfe6e706674f421c
overist <wj@over.ist>
2025-08-25 19:26:56 +0900

    chore: config set-up

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '25ac57c43eef80dca5b6c6e783510b34',
+  rootNotionPageId: '25ac57c43eef80bfa95cd019362c53f8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
